b4d2aeb3ebde3fde0aaefe7817f720e84acf67e2
suren <savetis.dev@gmail.com>
2024-10-16 21:26:21 +0400

    add prettier plugin

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,17 @@
-'use client';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useFormState } from 'react-dom';
-import * as actions from '@/actions';
+"use client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 
 export const description =
   "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
@@ -15,31 +21,41 @@ export default function LoginForm() {
     <Card className="mx-auto max-w-sm">
       <CardHeader>
         <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>Enter your email below to login to your account</CardDescription>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <form action={action}>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" name="email" placeholder="m@example.com" required />
-              <span>{formState.errors.email?.join(', ')}</span>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
+              <span>{formState.errors.email?.join(", ")}</span>
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
               <Input id="password" type="password" name="password" required />
-              <span>{formState.errors.password?.join(', ')}</span>
+              <span>{formState.errors.password?.join(", ")}</span>
             </div>
 
             {formState.errors._form ? (
-              <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div>
+              <div className="rounded border border-red-400 bg-red-200 p-2">
+                {formState.errors._form?.join(", ")}
+              </div>
             ) : null}
             <Button type="submit" className="w-full">
               Login
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{' '}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="underline">
               Sign up
             </Link>
